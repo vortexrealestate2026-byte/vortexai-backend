2fd7f4ceb9e983b568870e86fac5c774df3e2954
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-16 12:02:13 -0500

    Create models.py

---
+++ app/models.py
@@ -0,0 +1,22 @@
+from sqlalchemy import Column, Integer, String
+from app.database import Base
+
+class Property(Base):
+    __tablename__ = "properties"
+
+    id = Column(Integer, primary_key=True, index=True)
+    city = Column(String)
+    address = Column(String)
+    price = Column(Integer)
+    source = Column(String)
+
+
+class Vehicle(Base):
+    __tablename__ = "vehicles"
+
+    id = Column(Integer, primary_key=True, index=True)
+    city = Column(String)
+    make = Column(String)
+    model = Column(String)
+    price = Column(Integer)
+    source = Column(String)
