84d26f6da1de0a3fbad0b35cc0d0f645f0fda6cd
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-16 14:18:29 -0500

    Create buyers.py

---
+++ src/api/routes /buyers.py
@@ -0,0 +1,8 @@
+om ..schemas.buyer import BuyerCreate, BuyerUpdate
+
+def create_buyer(db, data: BuyerCreate):
+    buyer = db.create_buyer(data.dict())
+    return buyer
+
+def update_buyer(db, buyer_id: str, data: BuyerUpdate):
+    return db.update_buyer(buyer_id, data.dict(exclude_none=True))
