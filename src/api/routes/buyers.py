4fea95a3cdf38e82a23d056a1882f9050285d01c
vortexrealestate2026-byte <vortexrealestate2026@gmail.com>
2026-03-16 14:18:40 -0500

    Delete src/api/routes directory

--- src/api/routes/buyers.py
+++
@@ -1,8 +0,0 @@
-from ..schemas.buyer import BuyerCreate, BuyerUpdate
-
-def create_buyer(db, data: BuyerCreate):
-    buyer = db.create_buyer(data.dict())
-    return buyer
-
-def update_buyer(db, buyer_id: str, data: BuyerUpdate):
-    return db.update_buyer(buyer_id, data.dict(exclude_none=True))
--- src/api/routes/webhooks.py
+++
@@ -1,7 +0,0 @@
-def handle_esign_webhook(payload: dict):
-    print("[Webhook] eSign event received:", payload)
-    # TODO: update contract status in DB
-
-def handle_ingestion_webhook(payload: dict):
-    print("[Webhook] Ingestion event:", payload)
-    # TODO: trigger ingestion worker or update logs
